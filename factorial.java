8f201c2c8d14d51ffd8be219dafddf8d067fd5a1
Keerthan Gowda <MY_NAME@example.com>
2024-12-27 12:21:00 +0530

    factorial progm

---
+++ factorial.java
@@ -0,0 +1,16 @@
+package java_grooming;
+
+public class factorial
+    {
+	public static void main(String[] args) 
+	{
+		int no=10;
+		int f=1;
+		for(int i=no;i>=1;i--)
+		{
+		f=f*i;
+		}
+System.out.println(f);
+	}
+
+}
